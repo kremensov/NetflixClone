8f7cddab274f4328f5ffbdffd110896c3bc4f029
Rogério Martiniuc de Oliveira <kremensov@bol.com.br>
2021-02-22 15:37:06 -0300

    Projeto Bootcamp DIO

---
+++ js/owl/setup.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:20,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        300:{
+            items:4
+        },
+        600:{
+            items:6
+        },
+        1000:{
+            items:10
+        }
+    }
+})
---
+++ style/README.md
@@ -0,0 +1,2 @@
+# Projeto de Bootcamp da DIO
+### A criação de uma página HTML que simule o funcionamento da página oficial da NETFLIX, faz parte da avalição do Bootcamp oferecido pala DIGITAL INNOVATION ONE. 
